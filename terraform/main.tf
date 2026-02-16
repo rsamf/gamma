d18082d7e2282f6070c23a3a7d77b8ddf6577ad6
Richard Franklin <rsamfranklin@gmail.com>
2026-02-15 17:29:17 -0800

    Fixing deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,9 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-  }
-
-  backend "s3" {
-    bucket = "gamma-terraform-state"
-    key    = "gamma/terraform.tfstate"
-    region = "us-east-1"
   }
 }
 
